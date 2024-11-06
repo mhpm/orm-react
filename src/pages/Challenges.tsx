4f8408a4e07f4d33263b642ff057e1940a4b42e5
Michelle Perez Morales <michelle.perez@sngular.com>
2024-11-06 14:15:48 -0600

    🩹 - uncomment localstorage component

--- src/pages/Challenges.tsx
+++ src/pages/Challenges.tsx
@@ -4,7 +4,7 @@ import LocaStorageChallenge from '@/components/challenges/LocaStorage';
 const Test = () => {
   return (
     <div className="flex justify-center w-full p-10">
-      {/* <LocaStorageChallenge /> */}
+      <LocaStorageChallenge />
       <Accordion />
     </div>
   );
