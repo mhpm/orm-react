fc6f14d7822391b335034cf7f59a68bbeb19c342
Michelle Perez Morales <michelle.perez@sngular.com>
2024-12-31 14:52:48 -0600

    🐛 - add supabase apiKey in header instance

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -8,7 +8,19 @@ let supabaseInstance: ReturnType<typeof createClient> | null = null;
 
 export const getSupabaseClient = () => {
   if (!supabaseInstance) {
-    supabaseInstance = createClient(supabaseUrl, supabaseKey);
+    supabaseInstance = createClient(supabaseUrl, supabaseKey, {
+      global: {
+        fetch: async (url, options = {}) => {
+          // const token = await session?.getToken({ template: 'supabase' });
+          const headers = new Headers(options.headers);
+          headers.set('apikey', supabaseKey);
+          // if (token) {
+          //   headers.set('Authorization', `Bearer ${token}`);
+          // }
+          return fetch(url, { ...options, headers });
+        },
+      },
+    });
   }
 
   return supabaseInstance;
