89a3c55448df67cf08fc64330f03022250e39229
Michelle Perez Morales <michelle.perez@sngular.com>
2025-02-26 15:30:08 -0600

    Keep calm and commit

--- src/features/users/components/UserList.tsx
+++ src/features/users/components/UserList.tsx
@@ -1,4 +1,4 @@
-import { memo, useCallback, useMemo, useState } from 'react';
+import { memo, useCallback, useState } from 'react';
 import { User, UserResponse } from '../types/User';
 import { Link } from 'react-router';
 import { faker } from '@faker-js/faker';
